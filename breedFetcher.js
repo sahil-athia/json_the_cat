5d0805c6f71f576afe15197cf5de09168ee99704
Sahil Athia <sahilathia918@gmail.com>
2020-11-05 18:58:11 +0000

    added tests for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,23 @@
 const request = require('request');
-const args = process.argv.slice(2);
-let breed = args[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    throw new Error("Request could not be made");
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    throw new Error("Breed could not be found");
-  }
 
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedname, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedname}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+      //we are returning the contents of the call back in the event of an error(the callback in index and test)
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      return callback(null, data[0].description);
+      //we want to return null as error and the data as desc in our call back
+    } else {
+      return callback('Breed not found');
+      // if the description is undefined but there is no error, we let this message equal our error
+    }
+
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,25 @@
+const {fetchBreedDescription} = require('../breedFetcher');
+const {assert} = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      assert.equal(err, null);
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+      assert.equal(expectedDesc, desc.trim());
+      done();
+    });
+  });
+
+  it('returns null for an invalid breed, via callback', (done) => {
+    fetchBreedDescription("zxcvbnm", (err, desc) => {
+      assert.equal(err, 'Breed not found');
+
+      const expectedDesc = null;
+      assert.equal(expectedDesc, desc);
+
+      done();
+    });
+  });
+});
+
